1e65b060ef6822762aa5cfe93fe49478366ae226
ullash4 <programmerullash@gmail.com>
2022-08-06 22:47:13 +0600

    node js

--- Lesson5/index.js
+++ Lesson5/index.js
@@ -1,1 +1,8 @@
-console.log(5)
+const getRandomFruitsName = require('random-fruits-name');
+var moviesNames = require('movies-names');
+
+console.log(getRandomFruitsName()) // Apple - English is default language
+
+console.log(moviesNames.random())
+// console.log(moviesNames.random(2))
+
